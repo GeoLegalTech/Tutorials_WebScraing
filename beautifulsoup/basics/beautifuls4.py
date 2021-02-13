f8fc08d9a324d8663372b97bc17ecaac7e2ebc0a
Geomario <marm1984@gmail.com>
2021-02-13 21:31:21 +0100

    Encoding problem solved in windows

--- beautifulsoup/basics/beautifuls4.py
+++ beautifulsoup/basics/beautifuls4.py
@@ -13,7 +13,7 @@ download_url = requests.get(scifi_url)
 soup = BeautifulSoup(download_url.text, features="html.parser")
 
 # Create a Local Copy.
-with open("download_scifi.html", "w") as file:
+with open("download_scifi.html", "w", encoding="utf-8") as file:
 	file.write(soup.prettify())
 
 # Cleaning and preparing data
@@ -62,47 +62,47 @@ for element in head_table: # Selecting the first 5 titles
     print (column_label)
 
 
-# print ("---------")
-# print (table_columns)
+print ("---------")
+print (table_columns)
 
-# table_columns = []
-# for element in head_table[0 : 5]:
-#     column_label = element.get_text(separator=" ", strip=True)
-#     column_label =  column_label.replace(" ", "_")
-#     table_columns.append(column_label)
-#     print (column_label)
+table_columns = []
+for element in head_table[0 : 5]:
+    column_label = element.get_text(separator=" ", strip=True)
+    column_label =  column_label.replace(" ", "_")
+    table_columns.append(column_label)
+    print (column_label)
+
+print ("---------")
+print (table_columns)
 #
-# print ("---------")
-# print (table_columns)
-# #
-# regex=re.compile("_\[\w\]")
-# table_columns = []
-# for element in head_table[0 : 5]:
-#     column_label = element.get_text(separator=" ", strip=True)
-#     column_label =  column_label.replace(" ", "_")
-#     column_label =  regex.sub("", column_label)
-#     table_columns.append(column_label)
-#     print (column_label)
-# print ("---------")
-# print (table_columns)
-#
-#
-#
-# # Extract the table data
-# rows = table_60_3.select("tr")
-# print (rows)
-#
-# table_data = []
-# for index, element in enumerate(rows):
-#     if index > 0:
-#         row_list = []
-#         values = element.select("td")
-#         for value in values:
-#             row_list.append(value.text.strip())
-#         table_data.append(row_list)
-# print (table_data)
-#
-# #Creating the DataFrame
-# df = pd.DataFrame(table_data, columns=table_columns)
-# print (df.head())
-# print (df["Title"])
+regex=re.compile("_\[\w\]")
+table_columns = []
+for element in head_table[0 : 5]:
+    column_label = element.get_text(separator=" ", strip=True)
+    column_label =  column_label.replace(" ", "_")
+    column_label =  regex.sub("", column_label)
+    table_columns.append(column_label)
+    print (column_label)
+print ("---------")
+print (table_columns)
+
+
+
+# Extract the table data
+rows = table_60_3.select("tr")
+print (rows)
+
+table_data = []
+for index, element in enumerate(rows):
+    if index > 0:
+        row_list = []
+        values = element.select("td")
+        for value in values:
+            row_list.append(value.text.strip())
+        table_data.append(row_list)
+print (table_data)
+
+#Creating the DataFrame
+df = pd.DataFrame(table_data, columns=table_columns)
+print (df.head())
+print (df["Title"])
