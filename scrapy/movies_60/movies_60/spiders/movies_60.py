e97dea84a784c7ee6e3aec4e6c3cba8d3fd0df62
Geomario <mr@geolegaltech.com>
2021-02-15 15:05:45 +0100

    Running script for scrapy basic in wiki table

--- scrapy/movies_60/movies_60/spiders/movies_60.py
+++ scrapy/movies_60/movies_60/spiders/movies_60.py
@@ -1,15 +1,44 @@
+# Import modules
 import scrapy
 
 # Spider class for movies
-class movies_60(scrapy.Spider):
-    name = "movies_60" #all lower case & name it as the page
+# The conventon is to call, the class, after the domin it scrapes
 
-    def start_request(self):    #requiered method requests sesion
-    urls = ['https://en.wikipedia.org/wiki/List_of_science_fiction_films_of_the_1960s']
 
-    for url in urls:
-        yield scrapy.Request(url=url, callback=self.parse)
+class Movies_60Spider(scrapy.Spider):
+    # Good practice is to name itafter the domain we trying to crawl.
+    name = "movies_60"  # all lower case & name it as the page
 
-    def parse (self, response): #required method extracts data
-    #selectors
-    all_listings = response.xpath()
+    def start_request(self):  # requiered method requests sesion (start the sesio)
+        urls = [
+            'https://en.wikipedia.org/wiki/List_of_science_fiction_films_of_the_1960s']
+
+    def parse(self, response):  # required method extracts data
+        # selectors
+        all_listings = response.url.xpath("//table[@class='wikitable']")
+        for data in all_listings:
+            title_movies = data.xpath("//tr/td/i/a/@title")
+            yield {
+                "Title": title_movies
+            }
+    # def parse(self, response):
+    #     page = response.url("/")[-2]
+    #     filename = f'wiki-{page}.html'
+    #     with open(filename, 'wb') as f:
+    #         f.write(response.body)
+
+ # def parse(self, response):
+ #        items = response.xpath("//div[@class='flash_li']")
+ #        for product in items:
+ #            product_name = product.xpath(".//a[@class='flash_li_link']/text()").get()
+ #            product_sale_price = product.xpath(" .//div[@class='flash_li_price']/span/text()").get()
+ #            product_org_price = product.xpath(".//div[@class='flash_li_price']/del/text()").get()
+ #            product_url = product.xpath(".//a[@class='flash_li_link']/@href").get()
+ #            discount =  product.xpath(".//div[@class='category_li_off']/text()").get()
+ #             yield {
+ #                    'name': product_name,
+ #                    'sale_price': product_sale_price,
+ #                    'orginal_price': product_org_price,
+ #                    'url': product_url,
+ #                    'discount': discount
+ #             }
