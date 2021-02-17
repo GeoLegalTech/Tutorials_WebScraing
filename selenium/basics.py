cbb7c103d8036529afff1ee94344164ad69beb6b
Geomario <mr@geolegaltech.com>
2021-02-17 12:52:34 +0100

    On-click button working over pdf extraction, not link-object

--- selenium/basics.py
+++ selenium/basics.py
@@ -1,25 +1,36 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import time
+import requests
 
-browser = webdriver.Chrome("/Users/mr/Desktop/chromedriver")
+# Download and parse the html
+gazzete_url = "https://www.holzwickede.de/amtsblatt/index.php"
+
+# Download the html from the gazzete_url
+download_url = requests.get(gazzete_url)
+
+# Parse the html with beautiful soup and create object
+soup = BeautifulSoup(download_url.text, features="html.parser")
+
+# Create a Local Copy.
+with open("download_gazzete.html", "w") as file:
+	file.write(soup.prettify())
+
+# Path chromedriver & get url
+path = "/Users/mr/Desktop/chromedriver"
+browser = webdriver.Chrome(path)
 browser.get("https://www.holzwickede.de/amtsblatt/index.php")
+# browser.implicitly_wait(10)
+
+ban = WebDriverWait(browser,15).until(EC.element_to_be_clickable((By.XPATH,"//a[@id='cc_btn_accept_all']"))).click()
+
 
 elem = browser.find_element_by_xpath("//div[@id='content']/div[7]/table//form[@name='gazette_52430']/a[@href='#gazette_52430']")
 elem.click()
-time.sleep(0.2)
+# time.sleep(0.2)
 
-elem = browser.find_element_by_xpath("//*")
+out_html = browser.find_element_by_xpath("//*")
 print (elem.get_attribute("outerHTML"))
-
-
-# from selenium import webdriver
-#
-# URL = 'https://www.holzwickede.de/amtsblatt/index.php'
-# CSS_SELECTOR = '.close'
-#
-# browser = webdriver.Chrome("/Users/mr/Desktop/chromedriver")
-# browser.implicitly_wait(10)
-# browser.get(URL)
-# close = browser.find_element_by_css_selector(CSS_SELECTOR)
-# close.click()
-#
