d0f665d3ab89350fe60bb319a0d8048400a99bb1
Geomario <mr@geolegaltech.com>
2021-03-03 13:54:35 +0100

    Arragement and cleaning of repository

--- Routines/ulr_parse.py
+++ Routines/ulr_parse.py
@@ -1,37 +1,37 @@
 from urllib.parse import urlparse
 
-# url = "https://www.myeblaettle.de/frontend/mvc/issueFeed/groupsAndIssues?&position=0&group=1289&allMode=false&mode=public"
-# parts = urlparse(url)
-# # for query in range(1289, 1427):
-#     # print (query)
+url = "https://www.myeblaettle.de/frontend/mvc/issueFeed/groupsAndIssues?&position=0&group=1289&allMode=false&mode=public"
+parts = urlparse(url)
+# for query in range(1289, 1427):
+    # print (query)
 
-# print (parts) #url parts are printed
-# print (parts.geturl()) #the completed url is parsed
-# print (parts.query) # query division is added
-# print (type(parts.query)) # query is string
-#
-# # print (parts.query.split('=')) # the entire url is divided
-# print (parts.query.split('=')[2].split('&')) # split the query
-# print (parts.query.split('=')[2].split('&')[0])
-# # print (parts.query.split('&'))
-#
-# print ('-------')
-# n = parts.query.split('=')[2].split('&')[0]
-# print (n)
-#
-# # x is the entire query
-# x = parts.query
-# x = x.replace(n, str(1600))
-# print ("This is my new link")
-# print (x)
-#
-# print ("This is then my new link")
-# parts = parts._replace(query=x)
-# print (parts.geturl())
-#
-#
-# url = "https://www.myeblaettle.de/frontend/mvc/issueFeed/groupsAndIssues?&position=0&group=1289&allMode=false&mode=public"
-# parts = urlparse(url)
+print (parts) #url parts are printed
+print (parts.geturl()) #the completed url is parsed
+print (parts.query) # query division is added
+print (type(parts.query)) # query is string
+
+# print (parts.query.split('=')) # the entire url is divided
+print (parts.query.split('=')[2].split('&')) # split the query
+print (parts.query.split('=')[2].split('&')[0])
+# print (parts.query.split('&'))
+
+print ('-------')
+n = parts.query.split('=')[2].split('&')[0]
+print (n)
+
+# x is the entire query
+x = parts.query
+x = x.replace(n, str(1600))
+print ("This is my new link")
+print (x)
+
+print ("This is then my new link")
+parts = parts._replace(query=x)
+print (parts.geturl())
+
+
+url = "https://www.myeblaettle.de/frontend/mvc/issueFeed/groupsAndIssues?&position=0&group=1289&allMode=false&mode=public"
+parts = urlparse(url)
 
 
 base_url = "https://www.myeblaettle.de/frontend/mvc/issueFeed/groupsAndIssues?&position=0&group=1289&allMode=false&mode=public"
@@ -45,6 +45,16 @@ for issue_num in range(1289, 1427):
     print (url)
 
 
+
+url = 'https://www.myeblaettle.de/?group=1289'
+parts = urlparse(url)
+
+print (parts.query)
+print (parts.query.split('='))
+issu= parts.query.split('=')[1]
+print (parts.query.split('=')[1])
+
+print (parts._replace(issu, "1281")
     # print (url, query)
 
 
